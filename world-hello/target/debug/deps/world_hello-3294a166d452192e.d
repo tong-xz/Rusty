6e3925cd7b6f3e4c0a4e05988e7d2be62196fbed
X_Z <52037372+tong-xz@users.noreply.github.com>
2023-09-21 02:47:23 -0400

    test

---
+++ world-hello/target/debug/deps/world_hello-3294a166d452192e.d
@@ -0,0 +1,5 @@
+E:\Rusty\world-hello\target\debug\deps\world_hello-3294a166d452192e.rmeta: src\main.rs
+
+E:\Rusty\world-hello\target\debug\deps\world_hello-3294a166d452192e.d: src\main.rs
+
+src\main.rs:
---
+++ world-hello/target/debug/deps/world_hello-d454e0aaa95e309c.d
@@ -0,0 +1,5 @@
+E:\Rusty\world-hello\target\debug\deps\world_hello-d454e0aaa95e309c.rmeta: src\main.rs
+
+E:\Rusty\world-hello\target\debug\deps\world_hello-d454e0aaa95e309c.d: src\main.rs
+
+src\main.rs:
